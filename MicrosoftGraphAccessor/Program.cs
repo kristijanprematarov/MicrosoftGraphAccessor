57c749cb9104ceb7e0ddb5adf803d2e107039dfe
Kristijan Prematarov <Kristijan.Prematarov@opengi.mk>
2023-02-18 13:05:10 +0100

    Add project files.

---
+++ MicrosoftGraphAccessor/Program.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using System.Text.Json;
+
+var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+var tenantId = "";
+var clientId = "";
+var clientSecret = "";
+
+ClientSecretCredential? clientSecretCredential = new(tenantId, clientId, clientSecret);
+
+GraphServiceClient graphSeviceClient = new(clientSecretCredential, scopes);
+
+var users = await graphSeviceClient.Users
+                                                             .Request()
+                                                             .GetAsync();
+
+foreach (var user in users)
+{
+    Console.WriteLine(JsonSerializer.Serialize(user, new JsonSerializerOptions
+    {
+        WriteIndented = true,
+    }));
+}
+
+
+
